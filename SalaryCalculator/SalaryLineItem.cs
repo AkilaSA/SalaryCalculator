e59b7351626c4f782be5c102f88390ccbac8e40e
Akila Abeysundara <Akila.Abeysundara@Live.com>
2022-04-04 18:30:19 +0530

    Added support for percentage allowances or deductions

---
+++ SalaryCalculator/SalaryLineItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculator
+{
+    public class SalaryLineItem
+    {
+        public SalaryLineItem(string name, decimal amount, bool isPercentage = false)
+        {
+            Name = name;
+            IsPercentage = isPercentage;
+            Amount = amount;
+        }
+
+        public string Name { get; }
+        public decimal Amount { get; }
+        public bool IsPercentage { get; }
+
+        public decimal GetValue(decimal baseValue) => IsPercentage ? baseValue * Amount / 100 : Amount;
+    }
+}
