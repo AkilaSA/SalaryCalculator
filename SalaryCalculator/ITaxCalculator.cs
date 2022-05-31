a713936603b6e772956fc54b45d632f8391b0389
Akila Abeysundara <Akila.Abeysundara@Live.com>
2022-05-31 21:03:37 +0530

    Squashed commit of the following:
    commit c87c43b4c832eed8b9d1ee541ab89c3e593c6bfb
    Author: Akila Abeysundara <Akila.Abeysundara@Live.com>
    Date:   Tue May 31 21:02:47 2022 +0530
    
        Added plugable tax calculator support.

---
+++ SalaryCalculator/ITaxCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculator
+{
+    public interface ITaxCalculator
+    {
+        decimal CalculateTaxAmount(decimal basicSalary, List<SalaryLineItem>? fixedAllowances = null, List<SalaryLineItem>? otherFixedDeductions = null);
+        Task<decimal> CalculateTaxAmountAsync(decimal basicSalary, List<SalaryLineItem>? fixedAllowances = null, List<SalaryLineItem>? otherFixedDeductions = null, CancellationToken cancellationToken = default);
+    }
+}
